8fa15c64239ddf3829889863ccebbab51e69554c
shafwar <naufalshafi15@gmail.com>
2025-08-29 16:33:47 +0700

    Reset to minimal configuration - remove all test files

--- public/index-test.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "PHP is working on Railway!";
-echo "<br>PHP Version: " . PHP_VERSION;
-echo "<br>Current time: " . date('Y-m-d H:i:s');
-echo "<br>Server: " . $_SERVER['SERVER_SOFTWARE'] ?? 'unknown';
-echo "<br>Document Root: " . $_SERVER['DOCUMENT_ROOT'] ?? 'unknown';
-echo "<br>Script Name: " . $_SERVER['SCRIPT_NAME'] ?? 'unknown';
-?>
