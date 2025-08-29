dade7bdc9e36da5f93679a35bda7076321e326ca
shafwar <naufalshafi15@gmail.com>
2025-08-29 13:44:55 +0700

    Fix PAPERTRAIL syntax error in logging config

--- config/logging.php
+++ config/logging.php
@@ -87,9 +87,9 @@ return [
             'level' => env('LOG_LEVEL', 'debug'),
             'handler' => env('LOG_PAPERTRAIL_HANDLER', SyslogUdpHandler::class),
             'handler_with' => [
-                'host' => env('PAPERTRAIL_URL'),
-                'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'host' => env('PAPERTRAIL_URL', 'null'),
+                'port' => env('PAPERTRAIL_PORT', 'null'),
+                'connectionString' => env('PAPERTRAIL_URL') ? 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT') : null,
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],
