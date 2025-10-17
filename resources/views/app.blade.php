5c849915a4135129476006275930d076754a3c2e
shafwar <naufalshafi15@gmail.com>
2025-10-18 00:30:58 +0700

    🔧 BUILD: Add Cahaya Anbiya favicon and update assets - fix missing logo in browser tab

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,11 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Favicon - Cahaya Anbiya Logo -->
+        <link rel="icon" type="image/png" href="/cahayanbiyalogo.png">
+        <link rel="shortcut icon" type="image/png" href="/cahayanbiyalogo.png">
+        <link rel="apple-touch-icon" href="/cahayanbiyalogo.png">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
